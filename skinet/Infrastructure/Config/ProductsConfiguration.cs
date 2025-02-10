27e80254c2755ea9ec30b6177080c350d440e80e
mohanap1504 <mohanap1504@gmail.com>
2025-02-11 00:01:32 +0530

    API Basic test with API

---
+++ skinet/Infrastructure/Config/ProductsConfiguration.cs
@@ -0,0 +1,16 @@
+using System;
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Config;
+
+public class ProductsConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.Property(x => x.price).HasColumnType("decimal(18,2)");
+        builder.Property(x => x.Name).IsRequired();
+    }
+}
