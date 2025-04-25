6f0308e46309b34502c5fdd4b0cb8c1d2cf9c704
mohanap1504 <mohanap1504@gmail.com>
2025-04-25 15:52:06 +0530

    Client side error Handling and loading

--- skinet/client/src/app/app.routes.ts
+++ skinet/client/src/app/app.routes.ts
@@ -2,11 +2,17 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 import { ShopComponent } from './features/shop/shop.component';
 import { ProductdetailsComponent } from './features/shop/productdetails/productdetails.component';
+import { TestErrorComponent } from './features/test-error/test-error.component';
+import { NotFoundComponent } from './Shared/components/not-found/not-found.component';
+import { ServerErrorComponent } from './Shared/components/server-error/server-error.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path:'shop', component: ShopComponent},
     {path:'shop/:id', component: ProductdetailsComponent},
-    {path:'**', redirectTo: '', pathMatch: 'full'},
+    {path:'test-error', component: TestErrorComponent},
+    {path:'not-found', component: NotFoundComponent},
+    {path:'server-error', component: ServerErrorComponent},
+    {path:'**', redirectTo: 'not-found', pathMatch: 'full'},
 
 ];
